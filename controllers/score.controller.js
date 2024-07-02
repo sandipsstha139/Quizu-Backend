74779f812d7134769a110de9186915c8f371d2e5
pradeepOP <pradeepkazi38@gmail.com>
2024-07-03 01:59:10 +0545

    update on score controller

--- controllers/score.controller.js
+++ controllers/score.controller.js
@@ -5,30 +5,111 @@ import { User } from "../models/user.model.js";
 import AppError from "../utils/AppError.js";
 import { CatchAsync } from "../utils/catchAsync.js";
 
+// export const createScore = CatchAsync(async (req, res, next) => {
+//   const user = await User.findById(req.body.user);
+
+//   if (!user) {
+//     return next(new AppError("No User with that ID!", 404));
+//   }
+//   const quiz = await Quiz.findById(req.body.quiz);
+
+//   if (!quiz) {
+//     return next(new AppError("No Quiz with that ID!", 404));
+//   }
+
+//   const score = await Score.create(req.body);
+
+//   user.score.push(score._id);
+//   await user.save();
+
+//   res.status(200).json({
+//     status: "Success",
+//     message: "Score Created Successfully",
+//     data: {
+//       score,
+//     },
+//   });
+// });
+
 export const createScore = CatchAsync(async (req, res, next) => {
-  const user = await User.findById(req.body.user);
+  const {
+    user: userId,
+    quiz: quizId,
+    selectedOptions,
+    totalScore,
+    correctAnswers,
+    wrongAnswers,
+    notAnswered,
+  } = req.body;
 
-  if (!user) {
-    return next(new AppError("No User with that ID!", 404));
+  try {
+    // Find user
+    const user = await User.findById(userId);
+    if (!user) {
+      return next(new AppError("No User with that ID!", 404));
+    }
+
+    // Find quiz and populate questions
+    const quiz = await Quiz.findById(quizId).populate("questions");
+    if (!quiz) {
+      return next(new AppError("No Quiz with that ID!", 404));
+    }
+
+    // Calculate scores based on selected options
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+    let notAnswered = 0;
+
+    selectedOptions.forEach((option) => {
+      const question = quiz.questions.find(
+        (q) => q._id.toString() === option.questionId
+      );
+      if (!question) {
+        notAnswered += 1; // Count as not answered if the question is not found
+      } else if (option.selectedOption && option.selectedOption !== "") {
+        if (question.correct_option === option.selectedOption) {
+          correctAnswers += 1;
+        } else {
+          wrongAnswers += 1;
+        }
+      } else {
+        notAnswered += 1; // Count as not answered if no option selected
+      }
+    });
+
+    // Calculate total score
+    const totalScore = correctAnswers;
+
+    // Create score document
+    const score = await Score.create({
+      user: userId,
+      quiz: quizId,
+      score: totalScore,
+      correctAnswers,
+      wrongAnswers,
+      notAnswered,
+      selected_options: selectedOptions.map((option) => ({
+        question: option.questionId,
+        option: option.selectedOption,
+      })),
+    });
+
+    // Update user's scores
+    user.score.push(score._id);
+    await user.save();
+
+    // Send response
+    res.status(200).json({
+      status: "success",
+      message: "Score created successfully",
+      data: {
+        score,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating score:", error);
+    next(new AppError("Server error", 500)); // Handle server error
   }
-  const quiz = await Quiz.findById(req.body.quiz);
-
-  if (!quiz) {
-    return next(new AppError("No Quiz with that ID!", 404));
-  }
-
-  const score = await Score.create(req.body);
-
-  user.score.push(score._id);
-  await user.save();
-
-  res.status(200).json({
-    status: "Success",
-    message: "Score Created Successfully",
-    data: {
-      score,
-    },
-  });
 });
 
 export const getScoreById = CatchAsync(async (req, res, next) => {
